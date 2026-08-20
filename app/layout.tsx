d9351d65923151fbabb68969d86ac714abdb1e0a
Arun Negi <Arun-Negi989>
2026-08-20 17:02:01 +0530

    no index

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,12 +36,18 @@ const montserrat = Montserrat({
   weight: ["500", "600", "700"],
 });
 
-export const metadata: Metadata = {
-  title: "Yoga Teacher Training in Rishikesh - Best Yoga School in Rishikesh",
-  description:
-    "Yoga Teacher Training in Rishikesh - Best Yoga School in Rishikesh",
-};
+// export const metadata: Metadata = {
+//   title: "Yoga Teacher Training in Rishikesh - Best Yoga School in Rishikesh",
+//   description:
+//     "Yoga Teacher Training in Rishikesh - Best Yoga School in Rishikesh",
+// };
 
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 export default function RootLayout({
   children,
 }: {
